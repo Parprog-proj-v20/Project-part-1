a1c1365ddd971ce515339253251e5dd82a0455fd
Медянникова Полина <p.medyannikova>
2025-11-03 12:45:45 +0300

    Project-part-1 Tests Удален файл functional_tests.cpp, тесты из него перенесены в integration_tests.cpp. Создан файл system_tests, отредактирован CMakeLists.txt

--- tests/integration_tests.cpp
+++ tests/integration_tests.cpp
@@ -7,3 +7,21 @@ class IntegrationTest : public ::testing::Test {
 protected:
     ComputerRoom room;
 };
+
+
+/**
+ * @brief Тест 1: Проверяет, что статистика работает после работы студентов
+ */
+TEST_F(IntegrationTest, StatisticsAfterStudentActivity) {
+    std::thread student([this]) {
+        room.student_bahavior(1, 0);
+    }
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    room.stop();
+    student.join();
+
+    EXPECT_NO_THROW({
+        room.print_statistics();
+        });
+}

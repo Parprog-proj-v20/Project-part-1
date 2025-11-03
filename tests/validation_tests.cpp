42ebdf8ee9225407044672f6ae4ee26a7cd9c415
Медянникова Полина <p.medyannikova>
2025-11-03 10:50:24 +0300

    Project-part-1 В файл validation_tests.cpp точно добавлен 1 тест

--- tests/validation_tests.cpp
+++ tests/validation_tests.cpp
@@ -21,7 +21,7 @@ protected:
 };
 
 /**
- * @brief Проверяет, что не выходит больше, чем входит
+ * @brief Тест 1: Проверяет, что не выходит больше, чем входит
  */
 TEST_F(ValidationTest, NeverMoreExitsThanEnters) {
     const int num_students = 10;
@@ -46,3 +46,67 @@ TEST_F(ValidationTest, NeverMoreExitsThanEnters) {
     EXPECT_LE(students_exited.load(), students_entered.load());
     std::cout << "Вошло: " << students_entered << ", вышло: " << students_exited << std::endl;
 }
+
+/**
+ * @brief Тест 2: Проверяет, что система не зависает при полной загрузке
+ *
+ * Запускаем больше студентов, чем мест (проверка на deadlock). Также добавляем студентов из второй группы
+ */
+TEST_F(ValidationTest, NoDeadlockUnderFullLoad) {
+    std::vector<std::thread> students;
+
+    for (int i = 0; i < 40; ++i) {
+        students.emplace_back([this, i]() {
+            room.student_behavior(1, i % 20);
+            });
+    }
+    for (int i = 0; i < 30; ++i) {
+        students.emplace_back([this, i]() {
+            room.student_behavior(2, i % 15);
+            });
+    }
+
+    std::this_thread::sleep_for(std::chrono::seconds(10));
+    room.stop();
+
+    for (auto& student : students) {
+        EXPECT_TRUE(student.joinable()) << "Поток не joinable - возможен deadlock";
+        if (student.joinable()) {
+            student.join();
+        }
+    }
+
+    SUCCEED();
+}
+
+/**
+ * @brief Тест 3: Проверяет, что поведение системы корректно при конкуренции групп
+ *
+ * Запускаем равное количество студентов из обеих групп
+ */
+TEST_F(ValidationTest, GroupCompetitionHandledCorrectly) {
+    std::vector<std::thread> students;
+
+    for (int i = 0; i < 15; ++i) {
+        students.emplace_back([this, i]() {
+            room.student_behavior(1, i); 
+            });
+    }
+    for (int i = 0; i < 12; ++i) {
+        students.emplace_back([this, i]() {
+            room.student_behavior(2, i);
+            });
+    }
+
+    std::this_thread::sleep_for(std::chrono::seconds(8));
+
+    room.stop();
+    for (auto& student : students) {
+        if (student.joinable()) student.join();
+    }
+
+    EXPECT_NO_THROW({
+        room.print_statistics();
+        });
+}
+
